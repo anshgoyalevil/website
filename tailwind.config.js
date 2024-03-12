cb911f76149c90b6b6dbd3cf5dbe324667a6d46b
Ansh Goyal <anshgoyal1704@gmail.com>
2024-03-12 11:03:03 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
 
-// delete deprecated colors that we don't use
+// delete deprecated colors that we don't
 delete colors['lightBlue'];
 delete colors['warmGray'];
 delete colors['trueGray'];
